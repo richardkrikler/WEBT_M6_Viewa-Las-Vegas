b514f20d9805f361a2a3b17d5fbc88429acbd48f
Richard Krikler <8112@htl.rennweg.at>
2022-04-12 09:57:57 +0200

    updated design

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ $template = file_get_contents('templates/main.html');
 
 foreach ($hotels as $hotel) {
     foreach ($hotel->getKeyValuePairs() as $key => $value) {
-        print($key . ' - ' . $value . "<br/>");
         $template = preg_replace("/[#]{3}($key)[#]{3}/", $value, $template, 1);
         // $template = str_replace("###$key###", $value, $template);
     }
