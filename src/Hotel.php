db60f6bc9074bafd6a04f13f11eb47deb4c16375
Richard Krikler <57064056+RichardKrikler@users.noreply.github.com>
2022-04-12 17:00:29 +0200

    finalised design

--- src/Hotel.php
+++ src/Hotel.php
@@ -8,19 +8,22 @@ class Hotel
     private int $stars;
     private Location $location;
     private string $description;
+    private string $imgUrl;
 
     /**
      * @param string $name
      * @param int $stars
      * @param Location $location
      * @param string $description
+     * @param string $imgUrl
      */
-    public function __construct(string $name, int $stars, Location $location, string $description)
+    public function __construct(string $name, int $stars, Location $location, string $description, string $imgUrl)
     {
         $this->name = $name;
         $this->stars = $stars;
         $this->location = $location;
         $this->description = $description;
+        $this->imgUrl = $imgUrl;
     }
 
     /**
@@ -55,12 +58,21 @@ class Hotel
         return $this->description;
     }
 
+    /**
+     * @return string
+     */
+    public function getImgUrl(): string
+    {
+        return $this->imgUrl;
+    }
+
     public function getKeyValuePairs(): array
     {
         return array_merge([
             'NAME' => $this->getName(),
             'STARS' => $this->getStars(),
-            'DESCRIPTION' => $this->getDescription()
+            'DESCRIPTION' => $this->getDescription(),
+            'IMG_URL' => $this->getImgUrl()
         ], $this->getLocation()->getKeyValuePairs());
     }
 }
